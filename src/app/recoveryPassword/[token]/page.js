f312c070ab210176c7bf4aaf3d07a038b3f99296
vaibhavNettyfy <vaibhavt.nettyfy@gmail.com>
2023-11-21 12:51:48 +0530

    resetPassword

---
+++ src/app/recoveryPassword/[token]/page.js
@@ -0,0 +1,8 @@
+import RecoveryPassword from "@/components/recoveryPassword/RecoveryPassword";
+import React from "react";
+
+const page = () => {
+  return <RecoveryPassword />;
+};
+
+export default page;
