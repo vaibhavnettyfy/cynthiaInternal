68e3ec030e4eac7d7f438a69f5526300e7b9761d
vaibhavNettyfy <vaibhavt.nettyfy@gmail.com>
2023-12-05 16:29:59 +0530

    desginChangesDone

--- src/components/modal/CommingSoon.js
+++ src/components/modal/CommingSoon.js
@@ -2,34 +2,49 @@ import { Box, DialogContent, Stack, Typography } from "@mui/material";
 import React from "react";
 import CommonButton from "../common/Button";
 
-function CommingSoon() {
+function CommingSoon({ handleClose }) {
   return (
     <Box
-        width={"450px"}
-        height={"200px"}
-        sx={{ borderRadius: "24px", padding: "30px" }}
+      width={"450px"}
+      height={"260px"}
+      sx={{ borderRadius: "24px", padding: "30px" }}
+    >
+      <DialogContent
+        sx={{
+          height: "100%",
+          padding: "0px",
+          textAlign: "center",
+          display: "grid",
+          alignItems: "center",
+          justifyItems: "center",
+        }}
       >
-        <DialogContent
-          sx={{
-            height: "100%",
-            padding: "0px",
-            textAlign: "center",
-            display: "grid",
-            alignItems: "center",
-            justifyItems: "center",
-          }}
+        <Typography
+          fontSize={"25px"}
+          fontWeight={"700"}
+          lineHeight={"25px"}
+          marginBottom={2}
         >
+          Coming Soon
+        </Typography>
+
+        <Stack sx={{
+          alignItems: "center",
+          justifyItems: "center",
+        }}>
           <Typography
-            fontSize={"16px"}
+            fontSize={"18px"}
             fontWeight={"500"}
             lineHeight={"25px"}
             marginBottom={2}
           >
-            Comming Soon
+            We're working on this.you will be notified when it is available Stay
+            tuned !
           </Typography>
-          
-        </DialogContent>
-      </Box>
+          <CommonButton buttonName="Okay" onClick={() => handleClose()} style={{ borderRadius: "3px", padding: "8px" }}/>
+        </Stack>
+      </DialogContent>
+    </Box>
   );
 }
 
